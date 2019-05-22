4d2a642964a24fa5d0326d0ede1644eaf2ad4764
Alexander-Davidiak <davidjaksasha@gmail.com>
2019-05-22 19:46:36 +0300

    add

---
+++ run.py
@@ -0,0 +1,31 @@
+# Import required libraries.
+print("Loading the required Python modules ...\n")
+
+import os
+import cv2 as cv
+import glob
+
+from imp import get_predict_api, plot_bw_color_comparison, IMG_PATH
+
+# Load model
+
+print("Loading the model ...")
+predict, _ = get_predict_api()
+
+cwd = os.getcwd()
+print("Demonstrate colorization using images found in\n",
+      os.path.join(cwd, IMG_PATH), "\n")
+
+print("Please close each image (Ctrl-w) to proceed through the demonstration.\n")
+
+if __name__ == '__main__':
+    image_folder = 'images'
+
+    images = glob.glob(os.path.join(IMG_PATH, "*_bw.png"))
+    images.sort()
+
+    for image in images:
+        print("Colorize " + os.path.basename(image))
+        gray = cv.imread(image, 0)
+        out = predict(gray)
+        plot_bw_color_comparison(gray, cv.cvtColor(out, cv.COLOR_BGR2RGB))
